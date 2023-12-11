a50c832c4e3a5991dd90d10406972d673520c0ce
Priyansh-Kotak <priyanshkotak1@gmail.com>
2023-12-11 09:30:33 +0530

    added headless true and mobile number into the client_otp

--- index.js
+++ index.js
@@ -1,141 +1,3 @@
-// Method 1
-
-// const express = require("express");
-// const app = express();
-// const { Client, LocalAuth } = require("whatsapp-web.js");
-// const puppeteer = require("puppeteer");
-// const port = 3001;
-// const qrcode = require("qrcode-terminal");
-// const http = require("http");
-// const server = http.createServer(app);
-// const { Server } = require("socket.io");
-
-// const io = new Server(server, {
-//   cors: {
-//     origin: "http://localhost:3000",
-//     methods: ["GET", "POST"],
-//     allowedHeaders: ["my-custom-header"],
-//     credentials: true,
-//   },
-// });
-
-// app.get("/", (req, res) => {
-//   res.send("<h1>Hello world</h1>");
-// });
-
-// // Function to create the server
-// const startServer = () => {
-//   server.listen(port, () => {
-//     console.log("listening on *:", port);
-//   });
-// };
-
-// // Creating client for WhatsApp connection.
-// const client = new Client({
-//   authStrategy: new LocalAuth({
-//     clientId: "YOUR_CLIENT_ID",
-//   }),
-//   puppeteer: {
-//     headless: false,
-//     ignoreDefaultArgs: ["--disable-extensions"],
-//     args: ["--no-sandbox", "--disable-setuid-sandbox"],
-//   },
-// });
-// //   puppeteer: {
-// //     headless: "new",
-// //     ignoreDefaultArgs: ["--disable-extensions"],
-// //     args: ["--enable-gpu"],
-// //   },
-
-// // }
-
-// client.on("authenticated", () => {
-//   console.log("Client is authenticated on the server side");
-// });
-
-// client.on("qr", (qr) => {
-//   // Generate and scan this code with your phone
-//   console.log("QR RECEIVED", qr);
-//   qrcode.generate(qr, { small: true });
-// });
-
-// const createWhatsappSession = (socket, mobilenumber) => {
-//   const sixDigitNumberOtp = Math.floor(
-//     100000 + Math.random() * 90000
-//   ).toString();
-//   client.on("ready", () => {
-//     // client.getChats().then((chats) => {
-//     //   console.log(chats[0]);
-//     // });
-
-//     client.sendMessage(
-//       `91${mobilenumber}@c.us`,
-//       `Welcome to GreenField International School, your OTP is ${sixDigitNumberOtp}`
-//     );
-
-//     socket.emit("client_otp", sixDigitNumberOtp);
-
-//     socket.emit("client", "Sending a gift from the server to the client");
-
-//     console.log("Client is ready!");
-
-//     server.close(() => {
-//       startServer();
-//     });
-//   });
-
-//   client.initialize;
-// };
-
-// const createWhatsappOtp = (socket) => {};
-
-// io.on("connection", (socket) => {
-//   console.log("a user connected", socket?.id);
-//   socket.on("disconnect", () => {
-//     console.log("user disconnected");
-//   });
-
-//   socket.on("mobilenumber", (mobilenumber) => {
-//     console.log(
-//       "Displaying mobile to server from the client side",
-//       mobilenumber
-//     );
-
-//     createWhatsappSession(socket, mobilenumber);
-//   });
-
-//   // console.log("Otp for testing is ", sixDigitNumberOtp);
-
-//   socket.on("otp-funtion-trigger", (data) => {
-//     console.log(
-//       "Receiving data from the client to server from the otp function",
-//       data
-//     );
-
-//     const otp = createWhatsappOtp(socket);
-//   });
-
-//   socket.on("connected", (data) => {
-//     console.log("Connected to the server", data);
-//     // emit message from the server side
-
-//     socket.emit("hello", "Hello from server");
-//   });
-// });
-
-// // Handle uncaught exceptions and restart the server
-// process.on("uncaughtException", async (err) => {
-//   console.error("Uncaught Exception:", err);
-//   const browser = await puppeteer.launch();
-//   server.close(async () => {
-//     // await browser.close();
-//     client.startServer();
-//   });
-// });
-
-// // Start the server
-// startServer();
-
 // Method 2
 
 const express = require("express");
@@ -204,19 +66,12 @@ const createWhatsappSession = async (socket, mobilenumber) => {
     100000 + Math.random() * 90000
   ).toString();
 
-  console.log(
-    "Logging the otp inside the the createwhatsapp function",
-    sixDigitNumberOtp
-  );
-
   client.sendMessage(
     `91${mobilenumber}@c.us`,
     `Welcome to GreenField International School, your OTP is ${sixDigitNumberOtp}`
   );
 
-  console.log("Logging the otp ", sixDigitNumberOtp);
-
-  socket.emit("client_otp", sixDigitNumberOtp);
+  socket.emit("client_otp", sixDigitNumberOtp, mobilenumber);
 
   socket.emit("client", "Sending a gift from the server to the client");
 
@@ -225,7 +80,6 @@ const createWhatsappSession = async (socket, mobilenumber) => {
 
 client.initialize().catch((error) => {
   console.error("Client initialization error:", error);
-  // Handle initialization error as needed
 });
 
 io.on("connection", (socket) => {
@@ -243,20 +97,8 @@ io.on("connection", (socket) => {
     createWhatsappSession(socket, mobilenumber);
   });
 
-  // console.log("Otp for testing is ", sixDigitNumberOtp);
-
-  socket.on("otp-funtion-trigger", (data) => {
-    console.log(
-      "Receiving data from the client to server from the otp function",
-      data
-    );
-
-    const otp = createWhatsappOtp(socket);
-  });
-
   socket.on("connected", (data) => {
     console.log("Connected to the server", data);
-    // emit message from the server side
 
     socket.emit("hello", "Hello from server");
   });
@@ -264,9 +106,7 @@ io.on("connection", (socket) => {
 
 process.on("uncaughtException", async (err) => {
   console.error("Uncaught Exception:", err);
-  // const browser = await puppeteer.launch();
   server.close(async () => {
-    // await browser.close();
     startServer();
   });
 });
